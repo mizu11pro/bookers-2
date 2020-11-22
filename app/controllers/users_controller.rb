0a6292b6630ad455f345a2e3962dfcc4b1be29a7
mizu11pro <mizuakasa@gmail.com>
2020-11-22 15:51:10 +0000

    [11/22]

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def show
     @book = Book.new
     @user = User.find(params[:id])
+    #@post_images = @user.post_images.page(params[:page]),reverse_order
   end
 
   def edit
