8217a86807cbba1dd38c2ad4434238a09968ee33
mizu11pro <mizuakasa@gmail.com>
2020-11-23 14:11:35 +0000

    [11/23]

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,6 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   attachment :profile_image
 
-  validates :name, presence: { message: "Name is too short (minimum is 2 characters)"
-  }
+  validates :name, length: {minimum: 2, maximum: 20 }
+  validates :introduction, length: {maximum: 50 }
 end
