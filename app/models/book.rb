a083bc62a4d514d9d632d21dfc9b4cfe1e11b521
mizu11pro <mizuakasa@gmail.com>
2020-12-03 05:16:20 +0000

    [いいね コメント機能実装]

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,7 +1,14 @@
 class Book < ApplicationRecord
   belongs_to :user
  has_many :book_comments, dependent: :destroy
+ has_many :favorites, dependent: :destroy
 
   validates :title, presence: true
   validates :body, presence: true, length: {maximum: 200 }
+
+  def favorited_by?(user)
+      favorites.where(user_id: user.id).exists?
+    #  引数で渡されたユーザidがfavoritesテーブル内に存在(exists?)するかどうかを調べる
+    #  存在していればtrue,なければfalseを返すようにしている
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy ] do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorites, only: [:create, :destroy]
   end
 end
